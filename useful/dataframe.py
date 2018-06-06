82a243ef72004a35c7fb68b1c508246e04ba53ce
Karl Haviland <havkarl@users.noreply.github.com>
2018-06-05 23:38:30 -0400

    Additional examples

--- useful/dataframe.py
+++ useful/dataframe.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
+# read from excel
 df = pd.read_excel('sample_data_frame.xlsx', index_col=0)
 print(df)
+
+# add a column and apply a function
+df['multiple'] = df.units.apply(lambda x: 3*x)
+#print(df)
+
+# find the average
+average = df['units'].mean()
+#print('Average units: {0}'.format(average))
